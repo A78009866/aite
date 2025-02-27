4f34c1f8c51392b90c76a30604e06d20dff8239c
A78009866 <salemdopamine@gmail.com>
2025-02-27 15:32:03 +0100

    Updated settings.py for production and fixed issues

--- project/settings.py
+++ project/settings.py
@@ -124,7 +124,7 @@ import cloudinary.uploader
 import cloudinary.api
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME', 'duixjs8az'),
-    'API_KEY': os.getenv('CLOUDINARY_API_KEY', '855567725718793'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY', '143978951428697'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET', 'ENvvZcFPyEqpQdt3iSqsO5r51LQ'),
 }
 
